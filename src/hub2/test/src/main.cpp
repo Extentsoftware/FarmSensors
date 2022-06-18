500cabd2ff2b8d4b51fbc24f70392308f3e9c42e
extentsoftware <marcus.poulton@gmail.com>
2022-06-18 13:22:31 +0100

    test work

--- src/hub2/test/src/main.cpp
+++ src/hub2/test/src/main.cpp
@@ -1,119 +1,43 @@
-#include <Arduino.h>
+/*
+* Depend  https://github.com/sandeepmistry/arduino-LoRa
+* */
 
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define SerialMon Serial
-
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
-#define SerialAT Serial1
-
-#define TINY_GSM_MODEM_SIM7000
-#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
-#define SerialAT Serial1
-
-// See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[]  = "YOUR-APN";     //SET TO YOUR APN
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-#include <TinyGsmClient.h>
 #include <SPI.h>
 #include <SD.h>
-#include <Ticker.h>
+#include <LoRa.h>
 
-#define DUMP_AT_COMMANDS
-
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, SerialMon);
-TinyGsm modem(debugger);
-#else
-TinyGsm modem(SerialAT);
-#endif
-
-#define uS_TO_S_FACTOR      1000000ULL  // Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP       60          // Time ESP32 will go to sleep (in seconds)
-
-#define UART_BAUD           9600
-#define PIN_DTR             25
 #define PIN_TX              27
 #define PIN_RX              26
+#define UART_BAUD           115200
 #define PWR_PIN             4
 
 #define SD_MISO             2
 #define SD_MOSI             15
 #define SD_SCLK             14
 #define SD_CS               13
-#define LED_PIN             12
 
+#define LORA_RST            12
+#define LORA_DI0            32
+#define RADIO_DIO_1         33
+#define RADIO_DIO_2         34
+#define LORA_SS             5
+#define LORA_MISO           19
+#define LORA_MOSI           23
+#define LORA_SCK            18
 
-void enableGPS(void)
-{
-    // Set SIM7000G GPIO4 LOW ,turn on GPS power
-    // CMD:AT+SGPIO=0,4,1,1
-    // Only in version 20200415 is there a function to control GPS power
-    modem.sendAT("+SGPIO=0,4,1,1");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" SGPIO=0,4,1,1 false ");
-    }
-}
+#define BAND                470E6
 
-void disableGPS(void)
-{
-    // Set SIM7000G GPIO4 LOW ,turn off GPS power
-    // CMD:AT+SGPIO=0,4,1,0
-    // Only in version 20200415 is there a function to control GPS power
-    modem.sendAT("+SGPIO=0,4,1,0");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" SGPIO=0,4,1,0 false ");
-    }
-}
-
-void modemPowerOn()
-{
-    pinMode(PWR_PIN, OUTPUT);
-    digitalWrite(PWR_PIN, LOW);
-    delay(1000);    //Datasheet Ton mintues = 1S
-    digitalWrite(PWR_PIN, HIGH);
-}
-
-void modemPowerOff()
-{
-    pinMode(PWR_PIN, OUTPUT);
-    digitalWrite(PWR_PIN, LOW);
-    delay(1500);    //Datasheet Ton mintues = 1.2S
-    digitalWrite(PWR_PIN, HIGH);
-}
-
-
-void modemRestart()
-{
-    modemPowerOff();
-    delay(1000);
-    modemPowerOn();
-}
+uint32_t counter = 0;
+SPIClass SPI1(HSPI);
 
 void setup()
 {
-    // Set console baud rate
-    SerialMon.begin(115200);
+    Serial.begin(115200);
+    while (!Serial);
 
-    delay(10);
-
-    // Set LED OFF
-    pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, HIGH);
-
-    modemPowerOn();
-
-    Serial.println("========SDCard Detect.======");
-    SPI.begin(SD_SCLK, SD_MISO, SD_MOSI);
+    Serial.println("LoRa Sender");
+    // Specify pin to initialize SPI
+    SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
     if (!SD.begin(SD_CS)) {
         Serial.println("SDCard MOUNT FAIL");
     } else {
@@ -121,126 +45,31 @@ void setup()
         String str = "SDCard Size: " + String(cardSize) + "MB";
         Serial.println(str);
     }
-    Serial.println("===========================");
 
-    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
-
-
-    Serial.println("/**********************************************************/");
-    Serial.println("To initialize the network test, please make sure your LET ");
-    Serial.println("antenna has been connected to the SIM interface on the board.");
-    Serial.println("/**********************************************************/\n\n");
-
-    delay(10000);
+    //Specify pin to initialize SPI1
+    SPI1.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
+    LoRa.setSPI(SPI1);
+    LoRa.setPins(LORA_SS, LORA_RST, LORA_DI0);
+    Serial.printf("Setup Lora freq : %.0f\n", BAND);
+    if (!LoRa.begin(BAND)) {
+        Serial.println("LORA Begin FAIL");
+        while (1);
+    }
+    Serial.println("LORA Begin PASS");
 }
 
 void loop()
 {
-    String res;
+    Serial.print("Sending packet: ");
+    Serial.println(counter);
 
-    Serial.println("========INIT========");
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("hello ");
+    LoRa.print(counter);
+    LoRa.endPacket();
 
-    if (!modem.init()) {
-        modemRestart();
-        delay(2000);
-        Serial.println("Failed to restart modem, attempting to continue without restarting");
-        return;
-    }
+    counter++;
 
-    Serial.println("========SIMCOMATI======");
-    modem.sendAT("+SIMCOMATI");
-    modem.waitResponse(1000L, res);
-    res.replace(GSM_NL "OK" GSM_NL, "");
-    Serial.println(res);
-    res = "";
-    Serial.println("=======================");
-
-    Serial.println("=====Preferred mode selection=====");
-    modem.sendAT("+CNMP?");
-    if (modem.waitResponse(1000L, res) == 1) {
-        res.replace(GSM_NL "OK" GSM_NL, "");
-        Serial.println(res);
-    }
-    res = "";
-    Serial.println("=======================");
-
-
-    Serial.println("=====Preferred selection between CAT-M and NB-IoT=====");
-    modem.sendAT("+CMNB?");
-    if (modem.waitResponse(1000L, res) == 1) {
-        res.replace(GSM_NL "OK" GSM_NL, "");
-        Serial.println(res);
-    }
-    res = "";
-    Serial.println("=======================");
-
-
-    String name = modem.getModemName();
-    Serial.println("Modem Name: " + name);
-
-    String modemInfo = modem.getModemInfo();
-    Serial.println("Modem Info: " + modemInfo);
-
-    // Unlock your SIM card with a PIN if needed
-    if ( GSM_PIN && modem.getSimStatus() != 3 ) {
-        modem.simUnlock(GSM_PIN);
-    }
-
-
-    for (int i = 0; i <= 4; i++) {
-        uint8_t network[] = {
-            2,  /*Automatic*/
-            13, /*GSM only*/
-            38, /*LTE only*/
-            51  /*GSM and LTE only*/
-        };
-        Serial.printf("Try %d method\n", network[i]);
-        modem.setNetworkMode(network[i]);
-        delay(3000);
-        bool isConnected = false;
-        int tryCount = 60;
-        while (tryCount--) {
-            int16_t signal =  modem.getSignalQuality();
-            Serial.print("Signal: ");
-            Serial.print(signal);
-            Serial.print(" ");
-            Serial.print("isNetworkConnected: ");
-            isConnected = modem.isNetworkConnected();
-            Serial.println( isConnected ? "CONNECT" : "NO CONNECT");
-            if (isConnected) {
-                break;
-            }
-            delay(1000);
-            digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-        }
-        if (isConnected) {
-            break;
-        }
-    }
-    digitalWrite(LED_PIN, HIGH);
-
-    Serial.println();
-    Serial.println("Device is connected .");
-    Serial.println();
-
-    Serial.println("=====Inquiring UE system information=====");
-    modem.sendAT("+CPSI?");
-    if (modem.waitResponse(1000L, res) == 1) {
-        res.replace(GSM_NL "OK" GSM_NL, "");
-        Serial.println(res);
-    }
-
-    Serial.println("/**********************************************************/");
-    Serial.println("After the network test is complete, please enter the  ");
-    Serial.println("AT command in the serial terminal.");
-    Serial.println("/**********************************************************/\n\n");
-
-    while (1) {
-        while (SerialAT.available()) {
-            SerialMon.write(SerialAT.read());
-        }
-        while (SerialMon.available()) {
-            SerialAT.write(SerialMon.read());
-        }
-    }
+    delay(5000);
 }
