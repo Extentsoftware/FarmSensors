fe69eb63d87e35e45839529c1fd0616275e9004c
Marcus Poulton <marcus.poulton@exify.net>
2023-05-22 17:12:07 +0200

    updates to use i2c

--- src/hubHeltecWS/include/main.h
+++ src/hubHeltecWS/include/main.h
@@ -1,3 +1,2 @@
 void GetMyMacAddress();
-void startLoRa();
 void setup();
--- src/hubHeltecWS/src/main.cpp
+++ src/hubHeltecWS/src/main.cpp
@@ -17,24 +17,9 @@ int packetcount=0;
 
 const int connectionTimeout = 15 * 60000; //time in ms to trigger the watchdog
 
-#define RF_FREQUENCY        868E6           // Hz
-#define RF_BAND             125E3         
-#define TX_OUTPUT_POWER     20              // dBm
-#define SPREAD              12   
-#define CODERATE            5
-#define SYNCWORD            0 
-
-#define LORA_PREAMBLE_LENGTH                        8               // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0               // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  1
-#define LORA_FIX_LENGTH_PAYLOAD_OFF                 0
-#define LORA_IQ_INVERSION_ON                        false
-#define LORA_CRC                                    true
-#define LORA_FREQ_HOP                               false
-#define LORA_HOP_PERIOD                             0
-#define RX_TIMEOUT_VALUE                            1000
 #define BUFFER_SIZE                                 30 // Define the payload size here
 #define SYNCWORD                                    0
+#define CHANNEL                                     5
 
 #define DEFAULT_BROKER            "bongomqtt.uksouth.cloudapp.azure.com"
 
@@ -53,6 +38,13 @@ extern "C"
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
+
+void GetMyMacAddress()
+{
+  uint8_t array[6] { 0,0,0,0,0,0 };
+  esp_efuse_mac_get_default(array);
+  snprintf(macStr, sizeof(macStr), "%02x%02x%02x%02x%02x%02x", array[0], array[1], array[2], array[3], array[4], array[5]);
+}
 
 void connectToWifi()
 {
@@ -98,9 +90,9 @@ void WiFiEvent(WiFiEvent_t event)
       break;
 
     case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-      Serial.println("WiFi lost connection");
-      xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-      xTimerStart(wifiReconnectTimer, 0);
+      //Serial.println("WiFi lost connection");
+      //xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      //xTimerStart(wifiReconnectTimer, 0);
       break;
 
     default:
@@ -111,10 +103,9 @@ void WiFiEvent(WiFiEvent_t event)
 void onMqttConnect(bool sessionPresent)
 {
   Serial.println("Connected to MQTT broker: ");
-  char topic[32];
-  sprintf(topic, "bongo/%s", macStr);
-
-  uint16_t packetIdSub = mqttClient.subscribe(topic, 2);
+  //char topic[32];
+  //sprintf(topic, "bongo/%s", macStr);
+  //uint16_t packetIdSub = mqttClient.subscribe(topic, 2);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
@@ -126,6 +117,10 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
   if (WiFi.isConnected())
   {
     xTimerStart(mqttReconnectTimer, 0);
+  }
+  else
+  {
+    xTimerStart(wifiReconnectTimer, 0);
   }
 }
 
@@ -174,13 +169,6 @@ void onMqttPublish(const uint16_t& packetId)
   Serial.println(packetId);
 }
 
-void GetMyMacAddress()
-{
-  uint8_t array[6] { 0,0,0,0,0,0 };
-  esp_efuse_mac_get_default(array);
-  snprintf(macStr, sizeof(macStr), "%02x%02x%02x%02x%02x%02x", array[0], array[1], array[2], array[3], array[4], array[5]);
-}
-
 bool sendMQTTBinary(unsigned char *report, int packetSize, const char *subtopic)
 {
   Serial.printf("sendMQTTBinary::entered\n");
@@ -199,55 +187,49 @@ bool sendMQTTBinary(unsigned char *report, int packetSize, const char *subtopic)
       }
       return true;
   }
+  else
+    Serial.printf("MQTT GPRS Not connected!\n");
   return false;
 }
 
-void onReceive(int packetSize) 
-{  
-  Serial.printf("lora received %d bytes\n",packetSize);
-  float snr = LoRa.packetSnr();
-  float rssi = LoRa.packetRssi();
-  long pfe = 0; //LoRa.packetFrequencyError();
-  CayenneLPP lpp(packetSize + 20);
-  Serial.printf("Got %d snr:%f rssi:%f pfe:%ld\n",packetSize, snr, rssi, pfe); 
-
-  LoRa.readBytes(lpp._buffer, packetSize);
-
-  // add signal strength etc
-  lpp._cursor = packetSize;
-  lpp.addGenericSensor(CH_SNR, snr);
-  lpp.addGenericSensor(CH_RSSI, rssi);
-  lpp.addGenericSensor(CH_PFE, (float)pfe);  
-
-  sendMQTTBinary(lpp._buffer, lpp._cursor, "sensor");
-
-  Serial.printf("lora receive complete\n");
-}
-
-void startLoRa() {
-  Serial.printf("\nStarting Lora: freq:%lu enableCRC:%d coderate:%d spread:%d bandwidth:%ld txpower:%d\n"
-  , (unsigned long)RF_FREQUENCY, LORA_CRC, CODERATE, SPREAD, (long)RF_BAND, TX_OUTPUT_POWER);
-
-  LoRa.setPreambleLength(LORA_PREAMBLE_LENGTH);
-  LoRa.setSpreadingFactor(SPREAD);
-  LoRa.setCodingRate4(CODERATE);
-
-  if (LORA_CRC)
-      LoRa.enableCrc();
-    else 
-      LoRa.disableCrc();
-
-  LoRa.setSyncWord(SYNCWORD);
-  LoRa.setSignalBandwidth((long)RF_BAND);
- 
-  LoRa.onReceive(onReceive);
-  LoRa.receive();
-}
 
 void loop() {
-  // int packetSize = LoRa.parsePacket();
-  // if (packetSize>0)
-  //   onReceive(packetSize);
+  static long i=0;
+  i++;
+  if ( (i % 300000) == 0)
+  {
+    Serial.printf(" %d ",Wire1.available());
+  }  
+}
+
+void receiveEvent(int howMany)
+{
+  static int pkts =0;
+  pkts++;
+  char buf[32];
+  Serial.printf( buf, "I2C %d Pkt %d\n",howMany, pkts);
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 30, buf);
+  Heltec.display->display();
+
+  unsigned char buffer[ 64 ];
+  memset(buffer,0,sizeof(buffer));
+#if false  
+  int i=0;
+  while (Wire1.available()>0)
+  {
+    unsigned char c = Wire1.read();
+    buffer[i] = c;
+    Serial.printf("%02X ", c);
+  }    
+#else
+  Wire1.readBytes(buffer, howMany);
+  for(int i=0; i<howMany; i++)
+    Serial.printf("%02X ", buffer[i]);
+#endif
+  Serial.printf( "\n");
+
+  sendMQTTBinary(buffer, howMany, "sensor");
 }
 
 void setup()
@@ -256,26 +238,42 @@ void setup()
 
   while (!Serial && millis() < 5000);
 
+  Serial.printf("Begin i2c\n");
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  for (;;)
+  {
+    if (Wire1.begin(CHANNEL, 1 ,2 ,0 ))
+    {
+      Serial.printf("I2C started slave %d clock %d\n",CHANNEL, Wire1.getClock());
+      break;
+    }
+    Serial.printf("I2C failed to start\n");
+    delay(1000);
+  }
+  Wire1.onReceive(receiveEvent); 
+
+
   Heltec.begin(
       true /*DisplayEnable Enable*/, 
-      true /*Heltec.Heltec.Heltec.LoRa Disable*/, 
+      false /*Heltec.Heltec.Heltec.LoRa Disable*/, 
       true /*Serial Enable*/, 
-      true /*PABOOST Enable*/, 
-      (unsigned long)RF_FREQUENCY /*long BAND*/);
+      false /*PABOOST Enable*/, 
+      0 );
  
-  startLoRa();
+  GetMyMacAddress();
 
-  // Heltec.display->init();
-//  Heltec.display->flipScreenVertically();  
-  // Heltec.display->setFont(ArialMT_Plain_10);
-  // Heltec.display->clear();
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();  
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->clear();
   
-  // Heltec.display->drawString(0, 0, "VESTRONG");
-  // Heltec.display->drawString(0, 10, "WIFI Listen");
-  // Heltec.display->drawString(0, 20, "LORA Listen");
-  // Heltec.display->display();
+  Heltec.display->drawString(0, 0, "VESTRONG");
+  Heltec.display->drawString(0, 10, "WIFI Listen");
+  Heltec.display->drawString(0, 20, "LORA Listen");
+  Heltec.display->display();
 
-#ifdef WIFI
+
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0,
                                     reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(5000), pdFALSE, (void*)0,
@@ -285,14 +283,15 @@ void setup()
 
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
+  //mqttClient.onSubscribe(onMqttSubscribe);
+  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  //mqttClient.onMessage(onMqttMessage);
   mqttClient.onPublish(onMqttPublish);
 
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
   connectToWifi();
-#endif  
+
+
   Serial.printf("End of setup\n");
 }
